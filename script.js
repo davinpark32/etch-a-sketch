e686c33ad3675e7d31e1b1d12a771e4c1dc83510
Davin <145159926+davinpark32@users.noreply.github.com>
2023-10-08 13:36:57 +0100

    add function that generates grid

--- script.js
+++ script.js
@@ -12,8 +12,7 @@ container.style.cssText = "display: flex; flex-wrap: wrap; width: 180px; justify
 
 gridBtn.addEventListener('click', () => {
     let size = prompt('Size');
-    let existingContainer = document.getElementById('container');
-    
+    let existingContainer = document.getElementById('testContainer');
     if (existingContainer) {
         document.body.removeChild(existingContainer);
     }
@@ -21,21 +20,22 @@ gridBtn.addEventListener('click', () => {
 });
 
 function createGrid(size) {
-    let container = document.createElement('div');
-    container.setAttribute("id", "container");
-    document.body.appendChild(container);
-    container.style.cssText = "display: flex; flex-wrap: wrap; justify-content: center; align-items: center;";
-    container.style.width = size * 10 + 20 + 'px';
-
-    for (i = 0; i < size*size; i++) {
-        let div = document.createElement("div");
-        div.setAttribute("class", "grid");
-        div.style.cssText =
-        "border: 1px solid black; margin-left: -1px; margin-top: -1px; width: 10px; height: 10px;";
-        div.addEventListener('mouseover', () => {
-            div.style.backgroundColor = 'black';
-        });
-        container.appendChild(div);
+    let gridSize = 960 /size + 'px'
+    const myContainer = document.createElement('div');
+    myContainer.setAttribute('id', 'testContainer');
+    myContainer.style.cssText = 'padding: 0px; margin: 0px; display: flex; justify-content: center; height: 960px; width: 960px; flex-wrap: wrap; border: 1px solid black'
+    document.body.appendChild(myContainer);
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let div = document.createElement('div');
+            div.setAttribute('class', 'newGrid');
+            div.style.cssText='border: 1px solid red; flex-shrink: 1; margin-left: -1px; margin-top: -1px; margin-right: -1px; margin-bottom: -1px';
+            div.style.width = gridSize;
+            div.style.height = gridSize;
+            div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = 'black'});
+            myContainer.appendChild(div);
+        }
     }
 }
 
